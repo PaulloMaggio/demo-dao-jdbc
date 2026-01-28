8f6a69a8c123a6848ddb9e2103ee3cb6cad2e3e1
Paulo Maggio <paullomagio@gmail.com>
2026-01-28 17:26:13 -0300

    Initial commit

---
+++ src/aplication/program.java
@@ -0,0 +1,24 @@
+package aplication;
+
+import java.util.Date;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class program {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1, "Books");
+		
+		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj );
+		
+		System.out.println(seller);
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
+	}
+
+}
