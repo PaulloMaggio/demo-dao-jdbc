a89a0d6ef6ba7750bc07e9f942eef0b3e2c61a54
Paulo Maggio <paullomagio@gmail.com>
2026-01-29 13:49:44 -0300

    Delete implementation

--- src/aplication/program.java
+++ src/aplication/program.java
@@ -39,5 +39,13 @@ public class Program {
 		seller.setName("Matha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 6: seller delete =====");
+		System.out.print("Enter id for delete teste: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete Completed");
+		
+		sc.close();
 	}
 }
